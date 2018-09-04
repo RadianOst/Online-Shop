dc04bfa95a1c0bbf4f481b4ebaf720a269bc115e
adrianOstromecki <bloodrian@gmail.com>
2018-09-04 13:28:28 +0200

    Add ProductCategory class and FeaturedProductCategory class. Implement constructors.

--- src/main/java/com/codecool/ProductCategory.java
+++ src/main/java/com/codecool/ProductCategory.java
@@ -1,8 +1,25 @@
 package com.codecool;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class ProductCategory{
+    private String name;
+    private Integer ID;
+    private static List<ProductCategory> categoryList = new ArrayList<ProductCategory>();
+
+    public ProductCategory(String name){
+        this.name = name;
+        ID = generateID();
+        categoryList.add(this);
+    }
+
     @Override
     public String toString() {
-        return "kategoria";
+        return name;
+    }
+
+    private Integer generateID(){
+        return categoryList.size();
     }
 }

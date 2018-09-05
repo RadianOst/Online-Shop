21fc5d93fe6cb3121cbfb408f9908018016f7394
dardomanski <dardomanski@gmail.com>
2018-09-05 17:11:16 +0200

    implemented main methods

--- src/main/java/com/codecool/controller/OrderController.java
+++ src/main/java/com/codecool/controller/OrderController.java
@@ -1,6 +1,5 @@
 package com.codecool.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.codecool.Product;
@@ -8,26 +7,36 @@ import com.codecool.order.Order;
 import com.codecool.view.OrderView;
 
 public class OrderController {
-    List<Order> orders;
+    private Order order;
+    private OrderView orderView;
 
     public OrderController() {
-        this.orders = new ArrayList<Order>();
     }
 
     public void makeOrder() {
-        Order order = new Order();
-        OrderView orderView = new OrderView(order);
-        boolean isFinished = false;
-
-        while (!isFinished) {
-
-        }
-
+        this.order = new Order();
+        this.orderView = new OrderView(this.order);
+        List<Product> products = orderView.chooseProducts();
+        addProductsToBasket(this.order, products);
     }
 
-    private List<Product> chooseProducts() {
-        List<Product> choosenProducts = new ArrayList<Product>();
-        return choosenProducts;
+    private void addProductsToBasket(Order order, List<Product> products) {
+        for (Product product : products) {
+            order.getBasket().addProduct(product);
+        }
+    }
+
+    public void printOrder() {
+        if (this.order != null) {
+            orderView.printOrder();
+        } else {
+            System.out.println("\nBasket is empty!\n");
+        }
+    }
+
+    public void addMoreProductsToBasket() {
+        List<Product> products = orderView.chooseProducts();
+        addProductsToBasket(this.order, products);
     }
 
 }

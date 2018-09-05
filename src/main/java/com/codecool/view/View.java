a36147ae0a6a0d153e60a822680b8776f7db8a99
dardomanski <dardomanski@gmail.com>
2018-09-05 13:14:22 +0200

    reorganized View class

--- src/main/java/com/codecool/view/View.java
+++ src/main/java/com/codecool/view/View.java
@@ -4,36 +4,7 @@ import com.codecool.Product;
 import java.util.*;
 import java.util.Scanner;
 
-/**
- * View
- * 
- */
-public abstract class View<Product> {
-
-    Product product;
-
-    public abstract void printView();
-    public abstract void printMenu();
-
-    public void printTitle( String title ){
-        System.out.println(title);
-    }
-
-    public void print( String string ){
-        System.out.println( string );
-    }
-   
-    public void print( ArrayList<Product> productList ){
-        for ( Product  product : productList ){
-            product.toString();
-        }
-    }
-
-    public void print( Product product ){
-        System.out.println(product.toString());
-    }
-
-
+public class View {
     Scanner scanner;
 
     public String getInput() {
